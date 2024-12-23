1b0be8d1951a0f4c75ed073a4ebcd7f8115ff881
Thiago Dias <thiago@thir.info>
2024-12-23 16:28:56 -0300

    feat(logging): enhance logging handler with parent-child support
    Introduced parent-child handler structure in the Track class to enable hierarchical logging. Added a parent_handler_uuid attribute and updated logging metadata accordingly. Also refined the initialization and default sink configuration for better handling of log records.

--- opsdataflow/track_flow/track_handler.py
+++ opsdataflow/track_flow/track_handler.py
@@ -7,30 +7,39 @@ from loguru import logger
 
 from opsdataflow.tools.uuid import generate_uuid4
 from opsdataflow.track_flow.constants import Constants
-from opsdataflow.track_flow.enums import Handler, LoggerLevel, TrackerLevel
-from opsdataflow.track_flow.handler_configuration import HandlerConfiguration
+from opsdataflow.track_flow.enums import Handler, LoggerLevel, TrackerGroup, TrackerLevel
 
 
 class Track:
-    """Handles logging functionality using the Loguru library.
+    """Logger class used to configure and handle logging activities.
 
-    This class provides a structured way to handle logging, offering functionalities to configure, log messages
-    at different levels, and access the logger instance. The TrackerHandler integrates with the Loguru framework
-    to customize log formatting, binding contextual data, and efficiently manage logging operations.
+    This class is designed to manage log records through the Loguru logging library. It includes mechanisms to set up
+    custom logging configurations, use hierarchical handling through parent and child loggers, and perform logging at
+    various levels with enriched metadata.
 
     Attributes:
-        parameters (dict[str, Any]): Configuration parameters for the logging handler.
-        __handler_uuid (str): Identifier for the logging handler instance.
-        _logger: Internal reference to the Loguru logger instance.
+        __handler (Handler): The handler instance associated with the logger, indicating the type of handling performed.
+        __handler_uuid (str): Unique identifier for the current handler.
+        __parent_handler_uuid (str | None): Identifier for the parent handler, if applicable.
+        _logger: The Loguru logger instance used for logging.
     """
 
-    def __init__(self, **kwargs: Any) -> None:
+    def __init__(self, handler: Handler, parent_handler_uuid: str | None = None, **kwargs: Any) -> None:
         """Logger class init method."""
-        self.parameters: dict[str, Any] = HandlerConfiguration(Handler.LOGGER).configure(**kwargs)
-        self.__handler_uuid: str = self.parameters[Constants.HANDLER_UUID_KEY]
+        self.__handler: Handler = handler
+        self.__handler_uuid: str = kwargs.get(Constants.HANDLER_UUID_KEY, "")
+        self.__parent_handler_uuid: str | None = parent_handler_uuid
         self._logger = logger
+
+        # initialize with default sink
+        self.__handler_default_sink_configuration()
+
+    def __handler_default_sink_configuration(self) -> None:
+        """Set the configuration for Reporter sink."""
         self._logger.bind(
-            uuid="",
+            event_uuid="",
+            parent_handler_uuid="",
+            parent_uuid="",
             handler_uuid="",
             handler_type="",
             generated_utc_timestamp="",
@@ -44,16 +53,17 @@ class Track:
         """Returns the Loguru Logger instance."""
         return self._logger
 
-    async def report(self, level: LoggerLevel | TrackerLevel, message: str, **kwargs: Any) -> None:
+    async def report(self, level: LoggerLevel | TrackerLevel | TrackerGroup, message: str, **kwargs: Any) -> None:
         """Unique point to apply changes and log."""
         now_utc: str = datetime.now(UTC).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 
         self._logger.log(
             level.name,
             message,
-            uuid=generate_uuid4(),
+            event_uuid=generate_uuid4(),
+            parent_handler_uuid=self.__parent_handler_uuid,
             handler_uuid=self.__handler_uuid,
-            handler_type=Handler.LOGGER.name,
+            handler_type=self.__handler.name,
             generated_utc_timestamp=now_utc,
             **kwargs,
         )
